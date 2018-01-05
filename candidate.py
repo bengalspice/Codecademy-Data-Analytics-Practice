b7a61772cdec2c3cab6055f53e643815ce720714
Sharmeen <32082105+bengalspice@users.noreply.github.com>
2018-01-04 20:21:51 -0500

    Add files via upload

---
+++ candidate.py
@@ -0,0 +1,27 @@
+import codecademylib
+import numpy as np
+from matplotlib import pyplot as plt
+
+survey_responses = ['Ceballos', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos','Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos',
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos',
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos',
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos']
+
+#2
+total_ceballos=sum([1 for n in survey_responses if n=='Ceballos'])
+#3
+percentage_ceballos=100*total_ceballos/len(survey_responses)
+print percentage_ceballos
+#4
+possible_surveys=np.random.binomial(70,0.54,size=10000)/70.0
+#5
+plt.hist(possible_surveys,bins=20, range=(0,1))
+plt.show()
+#6
+ceballos_loss_surveys=100 * np.mean(possible_surveys<0.5)
+print ceballos_loss_surveys
+#7
+large_survey=np.random.binomial(7000,0.2,10000)/7000.0
+#8
+ceballos_loss_new= 100 * np.mean(large_survey<50)
+print ceballos_loss_new
